5b6687a0530140456076f06903a1f78e0b495d5d
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2022-12-15 11:02:19 -0300

    feat: finishing design

--- src/components/CheckboxInput.tsx
+++ src/components/CheckboxInput.tsx
@@ -1,12 +1,13 @@
+import styles from './CheckboxInput.module.css'
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { CheckIcon } from "@radix-ui/react-icons";
 
 export function CheckboxInput() {
   return (
     <div>
-      <Checkbox.Root>
+      <Checkbox.Root className={styles.wrapper}>
         <Checkbox.Indicator>
-          <CheckIcon />
+          <CheckIcon className={styles.icon} />
         </Checkbox.Indicator>
       </Checkbox.Root>
     </div>
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,11 +1,18 @@
 import { CheckboxInput } from "./CheckboxInput";
 import styles from "./Task.module.css";
+import { HiOutlineTrash } from "react-icons/hi";
 
 export function Task() {
   return (
     <div className={styles.wrapper}>
-        <CheckboxInput/>        
-      <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+      <CheckboxInput />
+      <p>
+        Integer urna interdum massa libero auctor neque turpis turpis semper.
+        Duis vel sed fames integer.
+      </p>
+      <HiOutlineTrash
+        className={styles.trash}
+      />
     </div>
   );
 }
